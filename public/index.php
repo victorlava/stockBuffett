39eab8fc00c89d506b587ef133f796f648d1fa94
Victor Lava <victor.lava@aboutyou.com>
2021-04-03 11:55:34 +0300

    Moved symbol param into Quote constructor

--- public/index.php
+++ public/index.php
@@ -2,6 +2,6 @@
 
 require '../vendor/autoload.php';
 
-$test = new \StockBuffett\Action\AlphaVantage\Quote();
+$test = new \StockBuffett\Action\AlphaVantage\Quote('IBM');
 $test->initialize();
 echo $test->fire();
