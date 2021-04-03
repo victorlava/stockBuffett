63094e20fe96ac036720a86969566afaf33d9113
Victor Lava <victor.lava@aboutyou.com>
2021-04-03 11:57:43 +0300

    Added Daily stocks

--- public/index.php
+++ public/index.php
@@ -2,6 +2,6 @@
 
 require '../vendor/autoload.php';
 
-$test = new \StockBuffett\Action\AlphaVantage\Quote('IBM');
+$test = new \StockBuffett\Action\AlphaVantage\Daily('IBM');
 $test->initialize();
 echo $test->fire();
